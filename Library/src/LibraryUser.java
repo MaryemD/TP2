cd451f744b822180ae1ca78c69371172eb80922f
Maryem Dammak <mariem.dammak@insat.ucar.tn>
2024-01-01 18:12:05 +0100

    fist

---
+++ Library/src/LibraryUser.java
@@ -0,0 +1,4 @@
+interface LibraryUser {
+	public void borrowBook(String title , String Author , Library library ); 
+    public void returnBook(Book b,Library library ); 
+}
---
+++ Music/src/User.java
@@ -0,0 +1,8 @@
+
+
+interface User {
+	String getUsername();
+	void listen(Song s ) ; 
+    void addToPlaylist(Song s ); 
+    void listPlaylist(); 
+}
